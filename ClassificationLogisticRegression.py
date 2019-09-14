c64c33a6c638f66473c3954c9aa15cbc658d6e14
unknown <thisisvikaas@gmail.com>
2019-09-14 19:21:03 +0530

    AddedSklearnMetrics

--- ClassificationLogisticRegression.py
+++ ClassificationLogisticRegression.py
@@ -6,7 +6,10 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 import warnings
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
+# Ignore all warnings
 warnings.filterwarnings("ignore")
 
 # Read the csv data
@@ -15,7 +18,6 @@ InputDataFrame = pd.read_csv('data.csv')
 
 # 1. Extract features values and Image Ids from the data:
 # In this 2 class classification problem Make B = 0 and M = 1
-
 def partition(x):
     if x == 'B':
         return 0
@@ -32,7 +34,7 @@ InputDataFrame['diagnosis'] = DiagnosisClass
 DiagnosisList = InputDataFrame['diagnosis']
 FinalDataFrame = InputDataFrame.drop('diagnosis', axis=1)
 # print(FinalDataFrame.head(5))
-#Handling Inf and NaN values
+# Handling Inf and NaN values
 FinalDataFrame[:] = np.nan_to_num(FinalDataFrame)
 
 # 2.Data Partitioning - 80% for Training and remaining for validation and testing
@@ -45,11 +47,10 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 # print("CV:",X_cv.shape,Y_cv.shape)
 # print("Test:",X_test.shape,Y_test.shape)
 
-
-
 # 3. Train using Logistic Regression
 # Use Gradient Descent for logistic regression to train the model using a group of hyperparameters.
 
+# 4. Tune hyper-parameters: Validate the regression performance of your model on the validation
 logistic = LogisticRegression()
 penalty = ['l1', 'l2']
 C = np.logspace(0, 4, 10)
@@ -62,5 +63,23 @@ best_model = clf.fit(X_train, Y_train)
 # Best Hyperparameters
 # print('Best Penalty:', best_model.best_estimator_.get_params()['penalty'])
 # print('Best C:', best_model.best_estimator_.get_params()['C'])
-C =  best_model.best_estimator_.get_params()['C']
+C = best_model.best_estimator_.get_params()['C']
 penalty = best_model.best_estimator_.get_params()['penalty']
+
+# 5. Test your machine learning scheme on the testing set:
+model = LogisticRegression(C=C, penalty=penalty)
+model.fit(X_train, Y_train)
+Y_predicted = model.predict(X_test)
+
+# accuracy: (tp + tn) / (p + n)
+accuracy = accuracy_score(Y_test, Y_predicted)
+print('Accuracy: %f' % accuracy)
+# precision tp / (tp + fp)
+precision = precision_score(Y_test, Y_predicted)
+print('Precision: %f' % precision)
+# recall: tp / (tp + fn)
+recall = recall_score(Y_test, Y_predicted)
+print('Recall: %f' % recall)
+# f1: 2 tp / (2 tp + fp + fn)
+f1 = f1_score(Y_test, Y_predicted)
+print('F1 score: %f' % f1)
